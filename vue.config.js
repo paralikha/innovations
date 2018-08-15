5fe61fed52ddd7140d407e6f504ef510ef10533b
John Lioneil Dionisio <john.dionisio1@gmail.com>
2018-08-15 10:24:54 +0800

    Setup files

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      // proxy all requests starting with /api to
+      '/api/v1': {
+        target: 'http://localhost:8000/api/v1',
+        changeOrigin: false,
+      },
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Authorization, X-Requested-With, Origin, X-Auth-Token, X-CSRF-Token, Content-type'
+    },
+  },
+}
