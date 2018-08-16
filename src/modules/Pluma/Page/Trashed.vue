1e5587d63599059e6623d327f533d6c788df4aa9
John Lioneil Dionisio <john.dionisio1@gmail.com>
2018-08-16 11:18:06 +0800

    Add Trashed page

--- src/modules/Pluma/Page/Trashed.vue
+++ src/modules/Pluma/Page/Trashed.vue
@@ -7,6 +7,13 @@
       <v-icon v-if="page.icon">{{ page.icon }}</v-icon>
       <v-toolbar-title>{{ __(page.title) }}</v-toolbar-title>
     </v-toolbar>
+    <v-container fluid grid-list-lg>
+      <v-layout row wrap>
+        <v-flex xs12>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius natus ea est nesciunt, sint harum quod saepe iusto, nostrum ad voluptas sunt magni iure quibusdam esse ipsa asperiores debitis incidunt.
+        </v-flex>
+      </v-layout>
+    </v-container>
   </section>
 </template>
 
