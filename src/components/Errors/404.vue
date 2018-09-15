14baf0517b6901424f0e77814c4ab108e8b73726
princessalto <princessellen0016@yahoo.com>
2018-09-15 22:34:48 +0800

    .

--- src/components/Errors/404.vue
+++ src/components/Errors/404.vue
@@ -99,10 +99,51 @@
 </style>
 
 <script>
-import store from '@/store'
 export default {
-  store,
+  name: 'Error404',
 
-  name: 'Error',
+  created () {
+    window.addEventListener('load', (event) => {
+      this.typewriter()
+    })
+  },
+
+  methods: {
+    typewriter () {
+      /* eslint-disable */
+      var aText = new Array(
+        "<span class='terminal__text__span'>The page you're looking for doesn't exist</span>",
+        "<span class='terminal__text__span'>Go back to <a href='/'>Home</a> instead",
+      );
+      var iSpeed = 50;
+      var iIndex = 0;
+      var iArrLength = aText[0].length;
+      var iScrollAt = 20;
+
+      var iTextPos = 0;
+      var sContents = '';
+      var iRow;
+
+      sContents =  ' ';
+      iRow = Math.max(0, iIndex-iScrollAt);
+      var destination = document.getElementById("typedtext");
+
+      while ( iRow < iIndex ) {
+        sContents += aText[iRow++] + '<br />';
+      }
+      destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+      if ( iTextPos++ == iArrLength ) {
+        iTextPos = 0;
+        iIndex++;
+        if ( iIndex != aText.length ) {
+          iArrLength = aText[iIndex].length;
+          setTimeout(this.typewriter(), 200);
+        }
+      }
+      else {
+        setTimeout(this.typewriter(), iSpeed);
+      }
+    }
+  }
 }
 </script>
