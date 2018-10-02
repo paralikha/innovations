b80fada8e60aa2487b2498437f01115769d2119b
Princess <princessellen0016@yahoo.com>
2018-10-02 16:16:24 +0800

    .

--- src/modules/Innovations/About/partials/Banner.vue
+++ src/modules/Innovations/About/partials/Banner.vue
@@ -1,15 +1,15 @@
 <template>
   <section>
     <v-card flat class="transparent hidden-sm-and-down">
-      <v-responsive :aspect-ratio="16/9">
-        <iframe class="web" src="/frames/about/web/About-Us-H.html" frameborder="0"></iframe>
-      </v-responsive>
+      <div class="iframe__container">
+        <iframe class="web" src="/frames/about/web/About-Us-H(Version2).html" frameborder="0"></iframe>
+      </div>
     </v-card>
 
     <v-card flat class="transparent hidden-md-and-up">
-      <v-responsive :aspect-ratio="16/9">
-        <!-- <iframe class="mobile" src="/frames/about/mobile/About-Us-M.html" frameborder="0"></iframe> -->
-      </v-responsive>
+      <div class="iframe__container">
+        <iframe class="mobile" src="/frames/about/mobile/About-Us-M(Version2).html" frameborder="0"></iframe>
+      </div>
     </v-card>
   </section>
 </template>
@@ -22,17 +22,3 @@ export default {
   name: 'Banner',
 }
 </script>
-
-<style lang="stylus" scoped>
-iframe.web {
-  width: 800px;
-  height: 83vh;
-}
-iframe.mobile {
-  width: 100%;
-  height: 1425px;
-}
-#canvas {
-  width: 100% !important;
-}
-</style>
