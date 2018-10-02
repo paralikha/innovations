a28079b468518e7193e6e4dff2e7bb9bc34df97e
Princess <princessellen0016@yahoo.com>
2018-10-02 16:39:24 +0800

    About Us

--- src/modules/Innovations/About/partials/Banner.vue
+++ src/modules/Innovations/About/partials/Banner.vue
@@ -2,13 +2,25 @@
   <section>
     <v-card flat class="transparent hidden-sm-and-down">
       <div class="iframe__container">
-        <iframe class="web" src="/frames/about/web/About-Us-H(Version2).html" frameborder="0"></iframe>
+        <iframe
+        class="web"
+        src="/frames/about/web/About-Us-H(Version2).html"
+        frameborder="0"
+        >
+      </iframe>
       </div>
     </v-card>
 
     <v-card flat class="transparent hidden-md-and-up">
-      <div class="iframe__container">
-        <iframe class="mobile" src="/frames/about/mobile/About-Us-M(Version2).html" frameborder="0"></iframe>
+      <div class="iframe-container">
+        <v-responsive :aspect-ratio="16/9">
+          <iframe
+            class="mobile"
+            src="/frames/about/mobile/About-Us-M(Version2).html"
+            frameborder="0"
+            >
+          </iframe>
+        </v-responsive>
       </div>
     </v-card>
   </section>
